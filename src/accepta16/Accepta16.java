f626efb037ccda48577c8288c9c96f5b081d9ef4
saucvazquez <saucvazquezgibanel@gmail.com>
2020-01-09 17:29:19 +0100

    Test_main

--- src/accepta16/Accepta16.java
+++ src/accepta16/Accepta16.java
@@ -1,36 +1,30 @@
 package accepta16;
+
 import java.util.Scanner;
 //prova
+
 public class Accepta16 {
 
-
+    //main
     public static void main(String[] args) {
-  
-         int num;
-         double sum,r;
-         
-         Scanner teclat = new Scanner(System.in);
-      
-         System.out.print("Ingrese Numero:");
-         num = Integer.parseInt(teclat.nextLine());
-      
-         sum = Math.sqrt(num);
-         r = sum;
-         
-      
-         if(r%2 ==0)
-         {
-            if(sum*sum == num)
-            {
-               System.out.print("Es cuadrado Perfecto");
-            }
-         }
-         else
-         {
-            System.out.print("No es");
-         }
-         
-      }   
-        
-    }
 
+        int num;
+        double sum, r;
+        Scanner teclat = new Scanner(System.in);
+
+        System.out.print("Ingrese Numero:");
+        num = Integer.parseInt(teclat.nextLine());
+
+        sum = Math.sqrt(num);
+        r = sum;
+
+        if (r % 2 == 0) {
+            if (sum * sum == num) {
+                System.out.print("Es cuadrado Perfecto");
+            }
+        } else {
+            System.out.print("No es");
+        }
+
+    }
+}
